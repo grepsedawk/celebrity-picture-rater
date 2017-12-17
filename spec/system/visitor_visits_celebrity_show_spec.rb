076615c41d725fcff753c709e1f2b8530a81c644
Alex Piechowski <alex@piechowski.io>
2017-12-17 16:14:56 -0600

    Add a picture count to the celebrity show page

--- spec/system/visitor_visits_celebrity_show_spec.rb
+++ spec/system/visitor_visits_celebrity_show_spec.rb
@@ -1,11 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Visitor visits root route' do
-  let!(:celebrity) { FactoryBot.create(:celebrity, name: 'Taylor Swift') }
+  let(:celebrity) { FactoryBot.create(:celebrity, name: 'Taylor Swift') }
+
+  before do
+    FactoryBot.create(:picture, celebrity: celebrity)
+    visit celebrity_path(celebrity)
+  end
 
   it "displays celebrity's name" do
-    visit celebrity_path(celebrity)
-
     expect(page.body).to include(celebrity.name)
+  end
+
+  it 'displays celebrity picture count' do
+    expect(page.body).to include("#{celebrity.pictures.count} pictures")
   end
 end
