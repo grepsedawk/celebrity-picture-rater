988d13128d431d9d5538a23468c30202e942db1f
Alex Piechowski <alex@piechowski.io>
2018-01-16 23:27:06 -0600

    Preload 2 extra votes in the background (#46)
    * Preload 2 extra votes in the background
    
    This allows for a smoother and more performant front end
    
    * Add a small params fetch refactor
    
    Thanks @jah2488

--- app/javascript/packs/voting.js
+++ app/javascript/packs/voting.js
@@ -3,5 +3,5 @@ import Elm from '../Voting'
 document.addEventListener('DOMContentLoaded', () => {
     const target = document.querySelector('#voting')
     const celebrityId = parseInt(target.dataset.celebrityId)
-    Elm.Main.embed(target, {celebrity_id: celebrityId})
+    Elm.Voting.embed(target, {celebrity_id: celebrityId})
 })
