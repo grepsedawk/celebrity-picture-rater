75428a4a0ec466085df183ee31f7ba214463bbfb
Matt Bettinson <mattbettinson@gmail.com>
2018-05-22 22:40:15 -0400

    Change spec to have two diff pictures

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -6,17 +6,18 @@ RSpec.describe Picture, type: :model do
   let(:celebrity) { FactoryBot.create(:celebrity, name: 'Taylor Swift') }
 
   it 'can be deleted when it has votes' do
-    FactoryBot.create_list(:picture, 1, celebrity: celebrity)
-    picture = Picture.first
+    FactoryBot.create_list(:picture, 2, celebrity: celebrity)
+    left = Picture.first
+    right = Picture.last
     vote = Vote.new
 
     vote.celebrity = celebrity
-    vote.left_picture_id = picture.id
-    vote.right_picture_id = picture.id
-    vote.chosen_picture_id = picture.id
+    vote.left_picture_id = left.id
+    vote.right_picture_id = right.id
+    vote.chosen_picture_id = left.id
     vote.save!
 
-    expect { picture.destroy }.not_to raise_error
-    expect(Picture.count).to eq 0
+    expect { left.destroy }.not_to raise_error
+    expect(Picture.count).to eq 1
   end
 end
